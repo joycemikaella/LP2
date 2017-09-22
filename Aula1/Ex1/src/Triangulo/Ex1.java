4a887c769f257f525b9d6c1eede5fbd62e977c09
Joyce Mikaella <Joyce Mikaella@LAPTOP-FGUBNUU3>
2017-09-22 11:26:53 -0300

    Exercicio1

---
+++ Aula1/Ex1/src/Triangulo/Ex1.java
@@ -0,0 +1,20 @@
+package Triangulo;
+
+public class Ex1 {
+
+	
+	public static void main (String args []) {
+		int i;
+		String x = "*" ;
+		
+		
+		for (i=0;i<5;i++) {
+		
+			
+			System.out.println (x);	
+			x+=x;
+		}
+		
+	}
+	
+}
